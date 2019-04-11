e70e06f7773787057488ef5280b22e7be0f0e550
Galin Kostov <galin.kostov@strv.com>
2019-04-11 11:22:05 +0200

    Add Multichain repo to this one. Rename 'node-front' to 'multichain-app'. Update the process of the startup script, but not the functions implementation yet

--- multichain-node/startup.sh
+++ multichain-node/startup.sh
@@ -6,7 +6,7 @@ function connect_to_existing_blockchain() {
   echo
   echo "*** Connecting to existing blockchain '$BLOCKCHAIN_NAME' at $PARENT_NODE_HOST:$PARENT_NODE_PORT"
 
-  CURRENT_NODE_ADDRESS=$(multichaind esmiley@$PARENT_NODE_HOST:$PARENT_NODE_PORT | grep 'connect,send,receive' | sed -e 's/.*grant \(.*\) connect.*/\1/')
+  CURRENT_NODE_ADDRESS=$(multichaind $BLOCKCHAIN_NAME@$PARENT_NODE_HOST:$PARENT_NODE_PORT | grep 'connect,send,receive' | sed -e 's/.*grant \(.*\) connect.*/\1/')
 }
 
 function create_new_blockchain() {
@@ -70,34 +70,91 @@ function grant_permissions_to_current_node(){
 
 # --------- Execution starts here ---------
 
-# Try to start the BC node. If error, then it is a fresh container with no BC node config on it.
-# Then try to connect to an existing node on the network.
-# -> If the response contains the string "connect,send,receive" it means the parent node exists and we need to grant
-# perms to the current node
-# --> Grant permissions
-# --> Then update the RPC credentials (the `multichain.conf` has been created already)
-# --> Then start the node
-# -> If not, then it's an error, which means it is the root (first ever) node of the BC.
-# --> Then create the blockchain
-# --> Start it in the background, so we can issue a command to stop it (so we can update the RPC credentials)
+# Check if the multichain blockchain node dir exists.
+# -> If yes:
+# --> start up the node
+# --> publish its IP to the container environment and the IP-seeder
+# -> If not:
+# --> then it is a fresh container with no BC node config on it. Proceed to the steps described below...
+#
+# Then get an IP of an existing node on the network from the IP-seeder.
+# -> If the response is "NO-IP", it means there are no IPs in the list, which means it is the first (root) node in the chain. Therefore:
+# --> create a new blockchain configuration
+# --> start it in the background, so the RPC credentials are generated
 # --> wait 2 seconds for it to start and then stop it
-# --> Update the RPC credentials
-# --> Start the node in the foreground
-start_blockchain_foreground || {
+# --> update the RPC credentials
+# --> start the node
+# --> publish its IP to the container environment and the IP-seeder
+#
+# -> Otherwise, the response should be an IP of an existing (parent) node. Then:
+# --> connect to that parent node. If unsuccessful, do 3 extra requests to the IP-seeder + connection attempts to parent node. If none successful - kill node
+# --> grant permissions for the current node
+# --> update the RPC credentials (the `multichain.conf` has been created already)
+# --> start the node
+# --> publish its IP to the container environment and the IP-seeder
 
-  connect_to_existing_blockchain
+BLOCKCHAIN_DIR="/root/.multichain/$BLOCKCHAIN_NAME"
 
-  if [ -z $CURRENT_NODE_ADDRESS ]; then
+if [ -d "$BLOCKCHAIN_DIR" ]; then
+  echo "*** Current Blockchain node already set up."
+
+  start_blockchain_background
+  get_current_node_ip
+  publish_ip_to_seeder
+else
+  echo "*** Current Blockchain node not set up yet."
+
+  get_ip_from_seeder
+
+  if [ $RES == "NO-IP" ]; then
+    echo
+    echo "*** No seed IPs registered yet. This is the root node of the Blockchain."
+
     create_new_blockchain
     start_blockchain_background
     sleep 2
     stop_blockchain
     sleep 2
     update_rpc_credentials
-    start_blockchain_foreground
+    start_blockchain_background
+    get_current_node_ip
+    publish_ip_to_seeder
   else
+    echo
+    echo "*** Root node IP provided: $RES"
+
+    PARENT_NODE_HOST=$RES
+    connect_to_existing_blockchain
+
+    # If the node doesn't succeed at connecting to the parent node, we enter the while-loop
+    THRESHOLD=3
+
+    while [ -z "$CURRENT_NODE_ADDRESS" ]; do
+      # If the IP list gets empty (because all IPs are of dead hosts) we don't want to handle here the scenario of creating a new node because it will be root node with no data, which means a whole new Blockchain. That means the DB is lost or the IP-seed is unresponsive/has lost data...
+
+      if [[ $THRESHOLD -lt 1 ]]; then
+        echo
+        echo "*** ERROR: Node could not connect to three different root nodes. Exiting..."
+
+        export LAUNCH_SERVICE=false
+        exit 1;
+      fi
+
+      check_ip_with_seeder $PARENT_NODE_HOST
+
+      get_ip_from_seeder
+      PARENT_NODE_HOST=$RES
+
+      connect_to_existing_blockchain
+
+      let THRESHOLD-=1
+    done
+
+    # The node successfully connected to a parent node, so proceed with initializing and starting up
     grant_permissions_to_current_node
     update_rpc_credentials
-    start_blockchain_foreground
+    start_blockchain_background
+    get_current_node_ip
+    publish_ip_to_seeder
   fi
-}
+fi
