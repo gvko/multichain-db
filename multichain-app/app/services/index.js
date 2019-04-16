ac1d0ca0eb08b12a6c870ffbab8c376104dfc113
Galin Kostov <galin.kostov@strv.com>
2019-04-16 14:10:08 +0200

    Add JSDoc for the functions

--- multichain-app/app/services/index.js
+++ multichain-app/app/services/index.js
@@ -2,6 +2,12 @@
 
 const createError = require('../util/create-error');
 
+/**
+ * Get general info about the node. Basically just call the info retrieval RPC endpoint of Multichain
+ *
+ * @param app
+ * @return {Promise<void>}
+ */
 async function getNodeInfo(app) {
   try {
     return await app.multichain.getInfo();
--- multichain-app/app/services/stream.js
+++ multichain-app/app/services/stream.js
@@ -2,6 +2,12 @@
 
 const createError = require('../util/create-error');
 
+/**
+ * List all data streams in the blockchain
+ *
+ * @param app
+ * @return {Promise<*>}
+ */
 async function listStreams(app) {
   try {
     return await app.multichain.listStreams();
@@ -10,11 +16,23 @@ async function listStreams(app) {
   }
 }
 
+/**
+ * List the items (records) in a certain stream
+ *
+ * @param app
+ * @param {string}  stream      The name of the stream for which to list the records
+ * @param {number}  itemsCount  The number of records to return
+ * @return {Promise<{count: number, data: Array}>}
+ */
 async function listStreamItems(app, stream, itemsCount) {
   try {
     const rawData = await app.multichain.listStreamItems({ stream, count: itemsCount || 9999 });
     const data = [];
 
+    /*
+     * Multichain requires the data in the streams to be stored in HEX-DEC format, so we need to convert it back
+     * to JSON.
+     */
     for (const row of rawData) {
       data.push(JSON.parse(hexToStr(row.data)));
     }
@@ -25,6 +43,13 @@ async function listStreamItems(app, stream, itemsCount) {
   }
 }
 
+/**
+ * Creates a new data stream
+ *
+ * @param app
+ * @param {string}  name  The name for the new stream
+ * @return {Promise<void>}
+ */
 async function createNewStream(app, name) {
   try {
     return await app.multichain.create({ type: 'stream', name, open: true });
@@ -33,6 +58,13 @@ async function createNewStream(app, name) {
   }
 }
 
+/**
+ * Subscribe the client to a certain data stream
+ *
+ * @param app
+ * @param {string}  stream  The name of the stream to subscribe to
+ * @return {Promise<PushSubscription>}
+ */
 async function subscribeToStream(app, stream) {
   try {
     return await app.multichain.subscribe({ stream });
@@ -41,6 +73,14 @@ async function subscribeToStream(app, stream) {
   }
 }
 
+/**
+ * Write new data to a stream
+ *
+ * @param app
+ * @param {string}                stream    The name of the stream to write (publish) to
+ * @param {object|string|number}  inputData The data to be written. Can be text, number or JSON
+ * @return {Promise<void>}
+ */
 async function publishDataToStream(app, stream, inputData) {
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const key = currentTimestamp.toString();
@@ -53,16 +93,28 @@ async function publishDataToStream(app, stream, inputData) {
   }
 }
 
-function strToHex(str) {
+/**
+ * Convert a string to hex-dec value
+ *
+ * @param {string}  input
+ * @return {string|string}
+ */
+function strToHex(input) {
   let hex = '';
 
-  for (let i = 0; i < str.length; i++) {
-    hex += '' + str.charCodeAt(i).toString(16);
+  for (let i = 0; i < input.length; i++) {
+    hex += '' + input.charCodeAt(i).toString(16);
   }
 
   return hex;
 }
 
+/**
+ * Convert a hex-dec value to string
+ *
+ * @param hexStr
+ * @return {Buffer}
+ */
 function hexToStr(hexStr) {
   return new Buffer.from(hexStr, 'hex');
 }
